c40836e4e03a2a4b6d93c69f064d837d4cf26a3e
Jim Strother <jim.strother@gmail.com>
2020-02-24 21:45:50 -0800

    Added: more to state and mutations

--- store/index.js
+++ store/index.js
@@ -1,30 +1,61 @@
-import { sum } from 'ramda';
+import { add, multiply, divide } from 'ramda';
+
+const yearlyTotal = multiply(12);
 
 export const state = () => ({
   totalAssets: '0.00',
   totalLiabilities: '0.00',
   totalMonthlyIncome: '0.00',
   totalMonthlyExpenses: '0.00',
+  totalAnnualIncome: '0.00',
+  totalAnnualExpenses: '0.00',
+  amountNeededForFire: '0.00',
+  annualPostFireIncome: '0.00',
+  estimatedWithdrawlRate: '0.04',
+  totalInvestments: '0.00',
+  monthlyInvestmentContribution: '0.00',
+  estimatedInvestmentInterestRate: '0.00',
+  compoundingPeriod: 'Annually',
 });
 
 export const mutations = {
-  SUM_ASSETS(state, value) {
-    state.totalAssets = sum([+state.totalAssets, value])
+  TOTAL_ASSETS(state, value) {
+    state.totalAssets = add(+state.totalAssets, value)
       .toFixed(2)
       .toString();
   },
-  SUM_LIABILITIES(state, value) {
-    state.totalLiabilities = sum([+state.totalLiabilities, value])
+  TOTAL_LIABILITIES(state, value) {
+    state.totalLiabilities = add(+state.totalLiabilities, value)
       .toFixed(2)
       .toString();
   },
-  SUM_MONTHLY_INCOME(state, value) {
-    state.totalMonthlyIncome = sum([+state.totalMonthlyIncome, value])
+  TOTAL_MONTHLY_INCOME(state, value) {
+    state.totalMonthlyIncome = add(+state.totalMonthlyIncome, value)
       .toFixed(2)
       .toString();
   },
-  SUM_MONTHLY_EXPENSES(state, value) {
-    state.totalMonthlyExpenses = sum([+state.totalMonthlyExpenses, value])
+  TOTAL_MONTHLY_EXPENSES(state, value) {
+    state.totalMonthlyExpenses = add(+state.totalMonthlyExpenses, value)
+      .toFixed(2)
+      .toString();
+  },
+  TOTAL_ANNUAL_INCOME(state) {
+    state.totalAnnualIncome = yearlyTotal(+state.totalMonthlyIncome)
+      .toFixed(2)
+      .toString();
+  },
+  TOTAL_ANNUAL_EXPENSES(state) {
+    state.totalAnnualExpenses = yearlyTotal(+state.totalMonthlyExpenses)
+      .toFixed(2)
+      .toString();
+  },
+  AMOUNT_NEEDED_FOR_FIRE(state) {
+    state.amountNeededForFire = divide(+state.totalAnnualIncome, +state.estimatedWithdrawlRate)
+      .toFixed(2)
+      .toString();
+  },
+  ANNUAL_POST_FIRE_INCOME(state) {
+    state.annualPostFireIncome = multiply(+state.amountNeededForFire, +state.estimatedWithdrawlRate)
       .toFixed(2)
       .toString();
   },
